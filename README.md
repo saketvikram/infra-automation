001ca700ce9a3d550a2a6dadfc03639388fd76da
Saket Vikram <saket.singh@tarento.com>
2018-06-06 17:03:54 +0530

    docker file change

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
-# test-devops
+# infra-devops
 This is for Infrastructure automation <br>
-<<<<<<< HEAD
-Using Ansible for Automation
 
-=======
 Using Ansible and Jenkins for Automation
->>>>>>> cacbbedd700f952d3b2a1e1efd39f4444fdb0e16
+
--- setupDocker.sh
+++ setupDocker.sh
@@ -6,7 +6,7 @@ sudo apt-get -y install \
     ca-certificates \
     curl \
     software-properties-common
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
